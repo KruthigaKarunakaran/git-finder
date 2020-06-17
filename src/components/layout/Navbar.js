c22a8ea67ddf1ca3a811a25ad79633fca03a5adb
Kruthiga Karunakaran <kruthiga@esighteyewear.com>
2020-06-17 17:16:46 -0400

    Create project

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import {PropTypes} from 'prop-types'
+export class Navbar extends Component {
+
+    static defaultProps = {
+        title : 'Github Finder',
+
+    }
+
+    static propTypes = {
+        title : PropTypes.string.isRequired
+    }
+
+    render() {
+        return (
+            <nav className='navbar bg-primary'>
+                <h1>
+                    <i class="fab fa-github"></i>
+                    {this.props.title}
+                </h1>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
