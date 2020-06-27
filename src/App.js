5eae4271c68cb180304eea54a2d60afe41781f24
Kruthiga Karunakaran <kruthiga@esighteyewear.com>
2020-06-27 17:00:28 -0400

    List and Passing States

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import User from './components/users/User';
 
 class App extends Component{
 
@@ -10,7 +10,9 @@ class App extends Component{
   return (
     <div className="App">
      <Navbar />
-     <UserItem />
+     <div className='container'>
+     <User/>
+     </div>
     </div>
   );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,16 +2,8 @@ import React, { Component } from 'react'
 
 class UserItem extends Component {
 
-   
-        state = {
-                id : '136',
-                login : 'simonjefford',
-                avatar_url : 'https://avatars2.githubusercontent.com/u/136?v=4',
-                html_url: 'https://github.com/simonjefford'
-        }
-      
     render() {
-        const {avatar_url, login , html_url} = this.state
+        const {avatar_url, login , html_url} = this.props.user
         return (
             <div className="card text-center">
                <img alt="imagePic" src= {avatar_url} className="round-img" style={{width : '60px'}}/>
